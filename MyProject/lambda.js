69eea93fb286bab8af338d8f1e443436c6d3e53a
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-06-05 09:09:39 +0800

    initial commit

---
+++ MyProject/lambda.js
@@ -0,0 +1,40 @@
+let AWS = require('aws-sdk');
+let codestar = new AWS.CodeStar();
+
+exports.handler = function(event, context, callback) {
+	codestar.listProjects({
+		maxResults: 5,
+		// nextToken: 'STRING_VALUE'
+	}).promise()
+	.then(data => {
+		console.log('Response -> data:', data);
+		let response = {
+			'statusCode': 200,
+			'headers': {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json'
+			},
+			'body': JSON.stringify({
+				'Data': data
+			}),
+			'isBase64Encoded': false
+		};
+		callback(null, response);
+	})
+	.catch(err => {
+		console.log('Response -> error:', err);
+		let response = {
+			'statusCode': err.statusCode,
+			'headers': {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json'
+			},
+			'body': JSON.stringify({
+				'Code': err.code,
+				'Message': err.message
+			}),
+			'isBase64Encoded': false
+		};
+		callback(null, response);
+  });
+}
